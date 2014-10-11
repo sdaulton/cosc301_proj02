e2e2e05b0d53b9d9c7a91e764e240bdb3b020777
Bria Vicenti <briavicenti@gmail.com>
2014-10-10 22:51:13 -0400

    Some progress!

--- main.c
+++ main.c
@@ -10,12 +10,159 @@
 #include <sys/stat.h>
 #include <poll.h>
 #include <signal.h>
+#include <stdbool.h>
 
+// SAM - If you have a better / more efficient / less ugly
+// tokenify please feel free to replace this one!
+char** tokenify(const char *s, int indicator) {
+    // Taken from lab 2, code by Bria Vicenti
+    // Will return a char array with one NULL element if no tokens are present. 
+    char *temp = strdup(s);
+    char* delim = NULL;
+
+    // Two delim cases to break it up into command sets / individual tokens.
+    if (indicator == 0) {
+    	delim = "\n \t";
+    }
+    else if (indicator == 1) {
+    	delim = ";";
+    }
+
+    if (NULL != strtok(temp, delim)) {
+    	char *temp2 = strdup(s);
+    	int count = 1;
+
+        while (NULL != strtok(NULL, delim)) {
+            count++;   
+        }
+
+        char** finished = malloc((count+1) * sizeof(char*)); // Account for final null slot.
+        char *p = strtok(temp2, delim);
+        char *p2 = strdup(p);
+        finished[0] = p2;
+
+        for (int i = 1; i < count; i++) {
+            p = strtok(NULL, delim);
+            p2 = strdup(p);
+            finished[i] = p2;
+        }
+
+        finished[count] = NULL;
+        free(temp);
+        free(temp2);
+        return finished;
+    }
+    else {
+        char** finished = malloc(sizeof(char*));
+        char *p = NULL;
+        finished[0] = p;
+        free(temp);
+        return finished;
+    }
+}
+
+void free_tokens(char **tokens) {
+    // Taken from Lab 2, code by Professor Sommers.
+    int i = 0;
+    while (tokens[i] != NULL) {
+        free(tokens[i]); // free each string
+        i++;
+    }
+    free(tokens); // then free the array
+}
+
+// Code by Professor Sommers, only here now for testing purposes.
+void print_tokens(char *tokens[]) {
+    int i = 0;
+    while (tokens[i] != NULL) {
+        printf("Token #%d:%s \n", i, tokens[i]);
+        i++;
+    }
+}
+
+void comment_handler(char* buffer) {
+	// Checks for a comment in the command line input and handles it in place.
+	for (int i = 0; i < strlen(buffer); i++) {
+		if (buffer[i] == '#') {
+			buffer[i] = '\0';
+			return;
+		}
+	}
+	return;
+}
+
+char*** cl_creator(char* buffer) {
+	// "Command list creator"
+	// Creates an array of arrays of strings to handle multiple commands.
+	// Feel free to clean! Not the neatest job handling extraneous issues
+	// but it works.
+	comment_handler(buffer);
+	char **untoked_cl = tokenify(buffer, 1);
+	int i = 0;
+	int orig_len = 0;
+	int command_count = 2;
+	while (untoked_cl[i] != NULL) {
+		orig_len++;
+		bool allspace = true;
+		for (int n = 0; n < strlen(untoked_cl[i]); n++) {
+			if (!isspace(untoked_cl[i][n])) {
+				allspace = false;
+				break;
+			}
+		}
+		if (!allspace) {
+			command_count++;
+		}
+		else {
+			untoked_cl[i] = NULL;
+		}
+		i++;
+	}
+
+	char ***cl = malloc(sizeof(char***) * command_count);
+	
+	int j = 0;
+	int k = 0; // j = tracker for untoked, k = write index
+
+	while (j < orig_len) {
+		if (untoked_cl[j] == NULL) {
+			j++;
+			continue;
+		}
+		char **temp = tokenify(untoked_cl[j], 0);
+		cl[k] = temp;
+		j++;
+		k++;
+	}
+	
+	cl[j] = NULL;
+	return cl;
+
+}
 
 int main(int argc, char **argv) {
-
-    // a blank slate :-)
+	/*over = false; // Checks whether the shell is ready to finish running.
+    while (!over) {
+    	printf("$: ");
+    	fflush(stdout);
+    	char buffer[1024];
+    	while (fgets(buffer, 1024, stdin) != NULL) {
+    		// Command line processing.	
+    	}
+    }
 
     return 0;
+    */
+
+    char test[] = "/path; /path -o -o; ; ; # /path2 -l -r; ;/path";
+    char ***cl = cl_creator(test);
+
+
+    int i = 0;
+    while (cl[i] != NULL) {
+    	printf("This is argument %d! \n", i);
+    	print_tokens(cl[i]);
+    	i++;
+    }
 }
 
